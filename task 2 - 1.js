da01d11a83b741108eb395f17e61223b9baf76cb
vit-khantimerov <92208142+vit-khantimerov@users.noreply.github.com>
2022-01-19 10:11:56 +0200

    1

---
+++ task 2 - 1.js
@@ -0,0 +1,30 @@
+/* ДЗ 2. Задача 1
+
+Напишите функцию `f`, которая возвращает куб числа. 
+Число передается параметром. 
+Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только число.
+
+```js
+f(2); // 8
+f('Content'); // Error: parameter type is not a Number
+*/
+
+
+
+//  Решение
+
+function f(a) {
+    if (typeof(a) === "number") {
+        return a * a * a;
+    } else {
+        throw new Error('Error: parameter type is not a Number'); // --- Проверить синтаксис.
+    }
+}
+
+console.log(f(8));
+console.log(f('Content'));
+
+// Результат
+512
+// Error: parameter type is not a Number
+// undefined
---
+++ task 2 - 2.js
@@ -0,0 +1,37 @@
+/* ДЗ 2. Задача 2
+
+Напишите функцию `f` которая возвращает сумму всех параметров. 
+Количество параметров может быть любым. 
+Данная функция должна обязательно содержать проверку входных параметров, потому что принимать она может только числа.
+
+```js
+f(1,2,3); // 6
+f(1,1,1,1,1,1,1,1); // 8
+f(1,2,'s',4); // Error: all parameters type should be a Number
+*/
+
+//  Решение
+
+function f(a) {
+    let sum = 0;
+    for (let i in arguments) {
+        if (typeof(arguments[i]) === "number") {
+            sum += arguments[i];
+        } else {
+            throw new Error('Error: all parameters type should be a Number'); // --- Проверить синтаксис.
+            break;                                                            // --- Удалить?
+        }
+    }
+    return sum;
+}
+
+console.log(f(1,2,3)); // 6
+console.log(f(1,1,1,1,1,1,1,1)); // 8
+console.log(f(1,2,'s',4)); // Error: all parameters type should be a Number
+
+
+// Результат
+// 6
+// 8
+// /home/HE080372KVG/JS-PB/HW 2/task 2 - 2.js:21
+//             throw new Error('Error: all parameters type should be a Number');
